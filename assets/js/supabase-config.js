5d14f7bab0b59e9e0e274e67402debfc931521c9
Marques Felipe <felipemedeirosmarques@hotmail.com>
2026-03-23 04:09:03 -0300

    Configura Supabase

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,4 +1,4 @@
 window.MARQUES_SUPABASE_CONFIG = window.MARQUES_SUPABASE_CONFIG || {
-  url: "",
-  anonKey: "",
+  url: "https://syzfewssgmlvlmpkactj.supabase.co",
+  anonKey: "sb_publishable_ZdqEjNFzWA4D8gLJHiqR1g_8BI7D4-n",
 };
